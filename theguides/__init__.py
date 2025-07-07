8b104f90d30bede0f24ca3ed872d4456970207fc
Crousei <72990211+Crousei@users.noreply.github.com>
2025-07-08 01:26:22 +0200

    renamed file

---
+++ theguides/__init__.py
@@ -0,0 +1,14 @@
+# plugins/test_plugin.py
+
+from discord.ext import commands
+
+class TestPlugin(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send("pong!")
+
+def setup(bot):
+    bot.add_cog(TestPlugin(bot))
